26432c007b55643decb3498a007fd283c75633df
sethcoder <defectiveseth@gmail.com>
2014-04-10 13:58:25 -0400

    update

--- module.nqt.php
+++ module.nqt.php
@@ -1,4 +1,22 @@
 <?
 include("include/lib.all.php");
+
+$RFS_ADDON_NAME="nqt";
+$RFS_ADDON_VERSION="2.0.0";
+$RFS_ADDON_SUB_VERSION="0";
+$RFS_ADDON_RELEASE="";
+$RFS_ADDON_DESCRIPTION="RFSCMS Network Query Tool Module";
+$RFS_ADDON_REQUIREMENTS="";
+$RFS_ADDON_COST="";
+$RFS_ADDON_LICENSE="";
+$RFS_ADDON_DEPENDENCIES="";
+$RFS_ADDON_AUTHOR="Seth T. Parson";
+$RFS_ADDON_AUTHOR_EMAIL="seth.parson@rfscms.org";
+$RFS_ADDON_AUTHOR_WEBSITE="http://rfscms.org/";
+$RFS_ADDON_IMAGES="";
+$RFS_ADDON_FILE_URL="";
+$RFS_ADDON_GIT_REPOSITORY="";
+$RFS_ADDON_URL=lib_modules_get_base_url_from_file(__FILE__);
+
 lib_menus_register("NQT","$RFS_SITE_URL/modules/rfscms-nqt/rfsnqt.php");
 ?>
